74b3603ac100df3ed79e2d6aa3fc671d6feafa3a
Alexander Axcenov <alexanderaxenov@mail.ru>
2020-07-30 08:48:02 +0800

    added sound for panel

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="panel-wrapper">
-    <div class="top-panel">
-      <div v-on:click="activateSimonBoard" class="panel top-left-panel" data-id="1">
-        <audio autoplay v-if="sequence">
-          <source src="assets/' + tile + '.mp3" type="audio/mp3" />
-        </audio>
-      </div>
-      <div @click="activateSimonBoard" class="panel top-right-panel" data-id="2"></div>
-    </div>
-    <div class="bottom-panel">    
-      <div @click="activateSimonBoard" class="panel bottom-left-panel" data-id="3"></div>
-      <div @click="activateSimonBoard" class="panel bottom-right-panel" data-id="4"></div>
-    </div>
+    <div
+      v-for="item in itemPanel"
+      :key="item.id"
+      :class="item.name"
+      @click.prevent="play(item.src)"
+      class="panel"
+    ></div>
     <div class="display">
       <button class="btn-start" @click="start">Start</button>
     </div>
@@ -19,17 +14,37 @@
 </template>
 
 <script>
-
 export default {
-  name: 'Panel',
-  components: {
-  },
+  name: "Panel",
+  components: {},
   data: () => ({
+    itemPanel: [
+      {
+        id: 1,
+        name: "top-panel-left",
+        src: require("../assets/1.mp3"),
+      },
+      {
+        id: 2,
+        name: "top-panel-right",
+        src: require("../assets/2.mp3"),
+      },
+      {
+        id: 3,
+        name: "bottom-panel-left",
+        src: require("../assets/3.mp3"),
+      },
+      {
+        id: 4,
+        name: "bottom-panel-right",
+        src: require("../assets/4.mp3"),
+      }
+    ],
     sequence: [],
-		copy: [],
-		round: 0,
-		active: true,
-		mode: 'normal',
+    copy: [],
+    round: 0,
+    active: true,
+    difficulty: "normal",
   }),
   methods: {
     start() {
@@ -37,48 +52,48 @@ export default {
       this.copy = [];
       this.round = 0;
       this.active = true;
-      this.newRound()
+      this.newRound();
     },
     newRound() {
       this.sequence.push(this.randomNumber());
       this.copy = this.sequence.slice(0);
     },
     randomNumber() {
-      return Math.floor((Math.random()*4)+1);
+      return Math.floor(Math.random() * 4 + 1);
     },
     registerClick(e) {
       const desiredResponse = this.copy.shift();
-      const actualResponse = $(e.target).data('tile');
-      this.active = (desiredResponse === actualResponse);
       this.checkLose();
     },
     checkLose() {
       if (this.copy.length === 0 && this.active) {
         this.deactivateSimonBoard();
         this.newRound();
-
-      } else if (!this.active) { 
+      } else if (!this.active) {
         this.deactivateSimonBoard();
         this.endGame();
-			}
+      }
     },
     endGame() {
-      $('p[data-action=lose]').show();
-      $($('[data-round]').get(0)).text('0');
     },
     activateSimonBoard(e) {
-      const id = e.target.getAttribute('data-id')
     },
-  }
-}
+    play(audio) {
+      const music = new Audio(audio);
+      music.play();
+    },
+  },
+};
 </script>
 
 <style lang="scss">
 .panel-wrapper {
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
+  width: 300px;
+  height: 300px;
+  flex-wrap: wrap;
   border-radius: 100%;
   position: relative;
 }
@@ -87,26 +102,29 @@ export default {
   width: 150px;
   height: 150px;
   cursor: pointer;
+  outline: none;
+  -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+  -webkit-tap-highlight-color: transparent;
 }
 .panel:hover {
   box-shadow: inset 0 0 20px 5px cyan;
 }
-.bottom-right-panel {
+.bottom-panel-right {
   border-bottom-right-radius: 100%;
   background-color: blue;
   opacity: 0.6;
 }
-.bottom-left-panel {
+.bottom-panel-left {
   border-bottom-left-radius: 100%;
   background-color: yellow;
   opacity: 0.6;
 }
-.top-right-panel {
+.top-panel-right {
   border-top-right-radius: 100%;
-  background-color: red;  
+  background-color: red;
   opacity: 0.6;
 }
-.top-left-panel {
+.top-panel-left {
   border-top-left-radius: 100%;
   background-color: green;
   opacity: 0.6;
